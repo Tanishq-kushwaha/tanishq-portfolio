bfc47f13362b89ddc804b3d5af2949f0e832e920
Tanishq Kushwaha <tanishqkushwaha09@gmail.com>
2026-06-13 16:10:13 +0530

    Refactor variable declarations in data.js to use const for better code clarity and consistency

--- data.js
+++ data.js
@@ -1,7 +1,7 @@
 
-let aboutMe = document.querySelector(".about-text");
+const aboutMe = document.querySelector(".about-text");
 aboutMe.innerText = "I am a Frontend Developer and SIDH-certified Junior Software Developer focused on building clean, responsive, and logic-first web experiences. Currently expanding my knowledge in computer science fundamentals and Cybersecurity basics, I enjoy solving algorithmic challenges and turning ideas into functional code. For me, web development is about engineering clean logic, secure structures, and writing code that works flawlessly across all devices.";
 
 
-let contact = document.querySelector("#contact-text");
+const contact = document.querySelector("#contact-text");
 contact.innerText ="I am always open to discussing new projects, creative ideas, or opportunities to collaborate on innovative web applications. Whether you want to build a clean user interface from scratch, optimize your existing codebase, or simply talk about programming logic and clean architecture, feel free to drop a message. Let's connect and build something valuable together.";
