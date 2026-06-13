308e4b77554dde12e553decb19c28a188fb89dc6
Tanishq Kushwaha <tanishqkushwaha09@gmail.com>
2026-06-13 15:46:18 +0530

    Update contact section and enhance about me description for clarity and engagement

--- script.js
+++ script.js
@@ -97,3 +97,7 @@ document.querySelectorAll('.nav-links a').forEach(link => {
 let aboutMe = document.querySelector(".about-text");
 
 aboutMe.innerText = "I am a Frontend Developer and NASSCOM-certified Junior Software Developer focused on building clean, responsive, and logic-first web experiences. Currently expanding my knowledge in computer science fundamentals and Cybersecurity basics, I enjoy solving algorithmic challenges and turning ideas into functional code. For me, web development is about engineering clean logic, secure structures, and writing code that works flawlessly across all devices.";
+
+
+let contact = document.querySelector("#contact-text");
+contact.innerText ="I am always open to discussing new projects, creative ideas, or opportunities to collaborate on innovative web applications. Whether you want to build a clean user interface from scratch, optimize your existing codebase, or simply talk about programming logic and clean architecture, feel free to drop a message. Let's connect and build something valuable together.";
